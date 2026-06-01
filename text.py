7fbca69203d887fe65307763494081f076482100
wademame <wademame2005@gmail.com>
2026-06-01 17:33:56 +0200

    sauvegarde avant fusion

--- text.py
+++ text.py
@@ -271,12 +271,7 @@ Smartphone Android :
 
 
 
-  cd ~ wget
-https://dl.google.com/android/repository/platform-tools-latest-linux.zip
-unzip platform-tools-latest-linux.zip echo 'export
-PATH="$HOME/platform-tools:$PATH"' >> ~/.bashrc source ~/.bashrc adb
---version
-Doit afficher : Android Debug Bridge version 1.0.41
+  
 
 """
 
